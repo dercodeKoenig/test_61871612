3a372636ff02d3b3f44a09744f441efb44b8a1ef
Marvin <marvineckhardt04092003@gmail.com>
2026-04-02 15:42:39 +0000

    .

--- upscaler.py
+++ upscaler.py
@@ -11,7 +11,7 @@ from SUPIR.util import create_SUPIR_model, PIL2Tensor, Tensor2PIL, convert_dtype
 from transformers import AutoProcessor, AutoModelForCausalLM
 from PIL import Image, ImageDraw, ImageFilter
 import gc
-
+allow_tiled = False
 
 model_id = 'microsoft/Florence-2-large'
 analyzer_model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, torch_dtype='auto').eval()
@@ -24,21 +24,24 @@ supir_model.ae_dtype = convert_dtype("bf16")
 supir_model.model.dtype = convert_dtype("fp16")
 print("SUPIR model loaded successfully!")
 
-supir_model_tiled = create_SUPIR_model('options/SUPIR_v0_tiled.yaml', SUPIR_sign="Q")
-supir_model_tiled.half()
-supir_model_tiled.ae_dtype = convert_dtype("bf16")
-supir_model_tiled.model.dtype = convert_dtype("fp16")
-supir_model_tiled.init_tile_vae(encoder_tile_size=3072, decoder_tile_size=192)
-print("Tiled SUPIR model loaded successfully!")
+supir_model_tiled = None
+if allow_tiled:
+    supir_model_tiled = create_SUPIR_model('options/SUPIR_v0_tiled.yaml', SUPIR_sign="Q")
+    supir_model_tiled.half()
+    supir_model_tiled.ae_dtype = convert_dtype("bf16")
+    supir_model_tiled.model.dtype = convert_dtype("fp16")
+    supir_model_tiled.init_tile_vae(encoder_tile_size=3072, decoder_tile_size=192)
+    print("Tiled SUPIR model loaded successfully!")
 
 
 def get_supir_model(tiled):
-    if(tiled):
+    if tiled:
         supir_model.cpu()
         torch.cuda.empty_cache()
         return supir_model_tiled.cuda()
     else:
-        supir_model_tiled.cpu()
+        if supir_model_tiled is not None:
+            supir_model_tiled.cpu()
         torch.cuda.empty_cache()
         return supir_model.cuda()
 
@@ -70,22 +73,30 @@ def upscale(img, scale, captions):
     LQ_img, h0, w0 = PIL2Tensor(LQ_ips, scale, min_size=512)
     LQ_img = LQ_img.unsqueeze(0).to("cuda")[:, :3, :, :]
 
+    if captions:
+        # Remove any trailing dots, commas, or spaces, then add a clean comma-space
+        # the program will just append prompt and p_p together
+        captions = captions.rstrip('. ,') + ', '
+    else:
+        captions = "" # Handle the empty case safely
+    
     print(img.size, scale, LQ_img.shape, captions)
 
     t1 = time.time()
 
     _, _, h_scaled, w_scaled = LQ_img.shape
     needs_tiling = False
-    if (w_scaled * h_scaled) > (2000 * 3000) and w_scaled >= 1024 and h_scaled >= 1024:
-        print(f"--- VRAM Protection: Using Tiled Model ({w_scaled}x{h_scaled}) ---")
-        needs_tiling = True
+    if allow_tiled:
+        if (w_scaled * h_scaled) > (2048 * 3072) and w_scaled >= 1024 and h_scaled >= 1024:
+            print(f"--- VRAM Protection: Using Tiled Model ({w_scaled}x{h_scaled}) ---")
+            needs_tiling = True
 
     model = get_supir_model(tiled=needs_tiling)    
 
-    control_scale = 0.97
+    control_scale = 0.98
     cfg_scale = 3.0   
-    s_noise = 1.02
-    cfg_scale_start = 1
+    s_noise = 0.98
+    cfg_scale_start = 1.2
     color_fix_type='Wavelet'
     restoration_scale = -1
 
@@ -93,7 +104,7 @@ def upscale(img, scale, captions):
         samples = model.batchify_sample(
             LQ_img,
             [captions],
-            num_steps=38,
+            num_steps=32,
             restoration_scale=restoration_scale,
             s_churn=0,
             s_noise=s_noise,
@@ -101,8 +112,8 @@ def upscale(img, scale, captions):
             control_scale=control_scale,
             seed=random.randint(0, 999999),
             num_samples=1,
-            p_p=' high quality, highly detailed, ultra HD, Cinematic, 32k, perfect without deformations.',
-            n_p='blurry, messy, noisy, deformed, lowres, low quality, grainy, distorted, artifacts, bad anatomy, malformed hands',
+            p_p='high quality, detailed, cinematic, perfect without deformations',
+            n_p='blurry, messy, noisy, deformed, lowres, low quality, grainy, distorted, artifacts, bad anatomy, malformed hands, malformed fingers, malformed teeth',
             color_fix_type=color_fix_type,
             use_linear_CFG=True,
             cfg_scale_start=cfg_scale_start,
@@ -119,9 +130,11 @@ def upscale(img, scale, captions):
 def enhance(image, scale, description = None):
 
     if description is None:
-        description = run_analysis("<DETAILED_CAPTION>", image)["<DETAILED_CAPTION>"]
+        #description = run_analysis("<DETAILED_CAPTION>", image)["<DETAILED_CAPTION>"]
+        description = run_analysis("<CAPTION>", image)["<CAPTION>"]
+        print("generated description:", description)
 
     upscaled = upscale(image, scale, description)
-    
     return upscaled
+    #return image
     
